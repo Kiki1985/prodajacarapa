f2887c16af030ea22fd90b7485b6780525060ac3
Kiki1985 <miroslav.dojchinovic@gmail.com>
2020-08-23 11:50:26 +0200

    changed title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Prodaja čarapa
                 </div>
 
                 <div class="links">
